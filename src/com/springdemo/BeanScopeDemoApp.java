18be739c02c02e0ac0d0354a132b2a7613a6b5af
Hryn <ahryn@ingrnet.com>
2016-11-18 16:30:54 +0100

    New code

---
+++ src/com/springdemo/BeanScopeDemoApp.java
@@ -0,0 +1,22 @@
+package com.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeDemoApp {
+
+	public static void main(String[] args) {
+		
+		// load the spring config file 
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanLifeCycle-applicationcontext.xml");
+		
+		// retrieve bean from spring container
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+		
+		System.out.println(theCoach.getDailyWorkout());
+		
+		// close the context
+		context.close();
+
+	}
+
+}
--- src/com/springdemo/TrackCoach.java
+++ src/com/springdemo/TrackCoach.java
@@ -24,5 +24,15 @@ public class TrackCoach implements Coach {
 		
 		return "Just do it:" + fortuneService.getFortune();
 	}
+	
+	// add an init method
+	public void doMyStrartupStuff(){
+		System.out.println("TrackCoach: inside method doMyStartupStuff");
+	}
+	
+	// add an destroy method
+	public void doMyCleanupStuff(){
+		System.out.println("TrackCoach: inside method doMyCleanupStuff");
+	}
 
 }
